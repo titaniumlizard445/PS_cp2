fd826aadf4a927e3d9266f56ca003f6ccbbff422
titaniumlizard445 <parker.swenson@ucas-edu.net>
2026-01-08 09:52:47 -0700

    commision and pushin'

---
+++ Individual_Projects/FinancialCalculations.py
@@ -0,0 +1,21 @@
+#PS 1st Calculator for finances
+
+#functions for each thing
+
+#$ per month thing inputs(goal, months)
+    #divide goal by months and output $/per month
+#Compound interest inputs(starting_balance, Interest, Time_in_years)
+    #for range(Time_in_years)
+        #multiply starting balance with interest
+    #return total
+#Budget Allocationer inputs(income,things_to_allocate_to)
+    #inner function for finding percentages of money used and deciding if you go out of range
+    #
+#Sales Price Calculator inputs(original_price,discount)
+    #subtract original price from original price x discount
+#Tip Calculator inputs(original_price,tip_percent)
+    #multiply original price with tip percent and add that to original price
+#User Interface 
+    #take in data and do things
+
+
