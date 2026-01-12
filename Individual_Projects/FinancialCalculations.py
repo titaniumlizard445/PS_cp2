f0ab3be4c57dd0be34688e8742d3060f3432dca3
titaniumlizard445 <parker.swenson@ucas-edu.net>
2026-01-12 09:53:43 -0700

    finish allocator and do UI

--- Individual_Projects/FinancialCalculations.py
+++ Individual_Projects/FinancialCalculations.py
@@ -3,10 +3,16 @@
 #functions for each thing
 
 #$ per month thing inputs(goal, months,weeks_or_months)
-def MonthsToGoal(goal,monthly,week_month):
+def MonthsToGoal(goal,monthly_pay):
     #divide goal by months and output $/per month
-    months = goal/monthly
-    return print(f"It will take {months} per month to reach {goal} in {months} months.")
+    def divide():
+        months = goal/monthly_pay
+        return months
+    divide()
+    round_months = round(divide())
+    if round_months < divide():
+        round_months+=1
+    return print(f"It will take {round_months} montsh to reach {goal} when paying {monthly_pay} per month.")
 
 #Compound interest inputs(starting_balance, Interest, Time_in_years)
 def CompoundInt(start_bal,Int,years):
@@ -25,15 +31,21 @@ def Allocator():
     categories = ["Rent/Mortgage","Transportation","Groceries","Utilities","Savings","Investments"]
     #asks for income and how many categories they would like to allocate to from preset categories
     income = input("Enter your monthly income: ").strip().lower()
+    
     print("\n\nCategories:\n1.Rent/Mortgage\n2.Transportation\n3.Groceries\n4.Utilities\n5.Savings\n6.Investments\n\n")
     #asks for percent to each category
     categoriesused = int(input("\nEnter number up to how many of these categories would you like to use: ").strip())
     
     #asks for percent of each category
-    for x in range(categoriesused):
-        #asks for each
-        print("Your mom")
-    #adds key:value pair to a dictionary for storage
+    for x in categoriesused:
+        #asks for each category's percent
+        percent = ""
+        while not percent.isnumeric():
+            percent = input(f"Enter in the percent for the {x} category here: ").strip()
+            if not percent.isnumeric():
+                print(f"Enter in just a number no % sign")
+        print(f"{categories[x]}:{percent}%")
+    
 #Sales Price Calculator inputs(original_price,discount)
 def Saleprice(og_price,discount):
     #subtract original price from original price x discount
@@ -47,7 +59,7 @@ def Tipcalc(og_price,tip_perc):
     added_price = og_price*tip_perc
     total = added_price+og_price
     return print(f"When you give {tip_perc}% of {og_price}$ then your total price will be{total}$")
-#User Interface 
-    #take in data and do things
 
+#User Interface 
+    #runs program and allows user to choose different stuff
 
--- Notes/lists.py
+++ Notes/lists.py
@@ -1,3 +1,17 @@
 #PS 1st more lists notes
 
+stuff = ["apple","computer","pencil","bed","notanidex"]
 
+#tuples
+other_stuff = ("car","keys","wires","plastic","rubber")
+coolspot = (2,-10)
+x,y = coolspot
+
+print(x)
+
+not_colors = {"andrew","whiteboard","outlet","backpack","projector","other projector","andrew"}
+
+for yourmom in not_colors:
+    if yourmom == "outlet":
+        print("That is dangerous")
+    print(yourmom)
