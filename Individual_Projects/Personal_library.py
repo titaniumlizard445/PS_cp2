494cf3fa1b614df158feb3505d0f6a0df33a705e
titaniumlizard445 <parker.swenson@ucas-edu.net>
2026-01-15 09:52:59 -0700

    started personal library

---
+++ Individual_Projects/Personal_library.py
@@ -0,0 +1,28 @@
+#PS 1st personal library
+
+#categorize video games
+#parts of an item: title, company, year, part of a series?
+
+library = {}
+
+#view library
+    #loop through set and print each item on an individual line
+
+#Add to library
+    #loop
+    #ask for and store all the criteria as listed above
+    #ask if user wants to add another
+
+#search library
+
+#remove from library
+    #loop
+    #Display library
+    #ask for which item to remove
+    #ask if done
+#edit existing item
+    #Display library
+
+#main
+
+
