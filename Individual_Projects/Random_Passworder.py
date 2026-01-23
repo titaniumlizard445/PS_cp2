14f9650a36c75a3e06e84df0da97310cc3131e39
titaniumlizard445 <parker.swenson@ucas-edu.net>
2026-01-23 09:53:20 -0700

    started random password generator pseudocode

---
+++ Individual_Projects/Random_Passworder.py
@@ -0,0 +1,19 @@
+#PS 1st random password generator
+#requirements length, upper/lower letters, numbers, and spec. characters
+
+#import random
+
+# letters (letter list, password)
+
+#non letters (numbers, special, password)
+
+#main
+    #loop
+        #list for letters special, and number with associated 
+        #input for how many characters they want to have in the password
+        #for characters in password 
+            #choose a character
+        #display password
+        #ask if user is done with program
+        #if yes
+            #leave
