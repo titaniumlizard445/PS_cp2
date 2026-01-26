edcf32bf8715c81cf0ccd6c1ba9770f762cda46e
titaniumlizard445 <parker.swenson@ucas-edu.net>
2026-01-26 09:53:27 -0700

    mostly coded random password generator

--- Individual_Projects/Random_Passworder.py
+++ Individual_Projects/Random_Passworder.py
@@ -2,17 +2,57 @@
 #requirements length, upper/lower letters, numbers, and spec. characters
 
 #import random
+import random
+# uppercaseletters (password)
 
-# letters (letter list, password)
+def upperletters(password): 
+    letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    password.append(random.choice(letters).upper())
+    return password
 
-#non letters (numbers, special, password)
+#lowercase letters (password)
+
+def lowerletters(password):
+    letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    password.append(random.choice(letters))
+    return password
+
+#Special Characters (password)
+
+def specials(password):
+    special = ["!","@","#","$","%","^","&","*"<"(",")","-","_","+","=","{","}","[","]",":",";","'",'"',"<",">",",",".","?","/","\\","|","`","~"]
+    password.append(random.choice(special))
+    return password
+
+#numbers (password)
+def numbers(password):
+    password.append(random.randrange(0,10))
+    return password
 
 #main
+def main():
     #loop
-        #list for letters special, and number with associated 
-        #input for how many characters they want to have in the password
-        #for characters in password 
-            #choose a character
+    while True:
+        #list for types of things
+        accepted_requirements =[]
+        #input for each of the requirements 
+        upper_req = input("Would you like upper case letters in your password?  (y/n): ").strip().lower()
+        if upper_req == "y":
+            accepted_requirements.append("uppers")
+        lower_req = input("Would you like lower case letters in your password?  (y/n): ").strip().lower()
+        if lower_req == "y":
+            accepted_requirements.append("lowers")
+        special_req = input("Would you like special characters in your password? (y/n): ").strip().lower()
+        if special_req == "y":
+            accepted_requirements.append("specials")
+        number_req = input("Would you like numbers in your password? (y/n): ").strip().lower()
+        if number_req == "y":
+            accepted_requirements.append("numbers")
+        length = input("how long would you like your password to be? (number): ").strip()
+        #for characters in password
+        for x in length:
+            #choose a type of character 
+            #run associated helper function
         #display password
         #ask if user is done with program
         #if yes
