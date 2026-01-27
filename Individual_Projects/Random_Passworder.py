03e64146014d4eb0293e179022fe5543110bf297
titaniumlizard445 <parker.swenson@ucas-edu.net>
2026-01-26 18:54:45 -0700

    Thank you for teaching me match case

--- Individual_Projects/Random_Passworder.py
+++ Individual_Projects/Random_Passworder.py
@@ -7,26 +7,30 @@ import random
 
 def upperletters(password): 
     letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-    password.append(random.choice(letters).upper())
+    upper_letter =random.choice(letters).upper()
+    password += upper_letter
     return password
 
 #lowercase letters (password)
 
 def lowerletters(password):
     letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-    password.append(random.choice(letters))
+    lower_letter = random.choice(letters)
+    password += lower_letter
     return password
 
 #Special Characters (password)
 
 def specials(password):
-    special = ["!","@","#","$","%","^","&","*"<"(",")","-","_","+","=","{","}","[","]",":",";","'",'"',"<",">",",",".","?","/","\\","|","`","~"]
-    password.append(random.choice(special))
+    special = ["!","@","#","$","%","^","&","*","(",")","-","_","+","=","{","}","[","]",":",";","'",'"',"<",">",",",".","?","/","\\","|","`","~"]
+    special_char = random.choice(special)
+    password += special_char
     return password
 
 #numbers (password)
 def numbers(password):
-    password.append(random.randrange(0,10))
+    number = str(random.randrange(0,10))
+    password += number
     return password
 
 #main
@@ -35,6 +39,7 @@ def main():
     while True:
         #list for types of things
         accepted_requirements =[]
+        rand_password = ""
         #input for each of the requirements 
         upper_req = input("Would you like upper case letters in your password?  (y/n): ").strip().lower()
         if upper_req == "y":
@@ -48,12 +53,32 @@ def main():
         number_req = input("Would you like numbers in your password? (y/n): ").strip().lower()
         if number_req == "y":
             accepted_requirements.append("numbers")
-        length = input("how long would you like your password to be? (number): ").strip()
+        length = int(input("how long would you like your password to be? (whole number): ").strip())
         #for characters in password
-        for x in length:
+        for x in range(length):
+            choice = ""
             #choose a type of character 
+            choice = random.choice(accepted_requirements)
             #run associated helper function
+            match choice:
+                case "":
+                    print("No requirements specified")
+                case "uppers":
+                    rand_password = upperletters(rand_password)
+                case "lowers":
+                    rand_password = lowerletters(rand_password)
+                case "specials":
+                    rand_password = specials(rand_password)
+                case "numbers":
+                    rand_password = numbers(rand_password)
         #display password
+        print(f"Your password is {rand_password}")
         #ask if user is done with program
-        #if yes
+        done = input("Would you like to generate another password? (y/n)").strip().lower()
+        #if done
+        if done == "n":
             #leave
+            print("goodbye")
+            break
+
+main()
