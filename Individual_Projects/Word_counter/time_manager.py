ab2bcf7f5cc8a31ca74d9cde241f2e9dd45ecc2a
titaniumlizard445 <parker.swenson@ucas-edu.net>
2026-02-21 16:07:17 -0700

    Add timestamp function to log word count and update time

--- Individual_Projects/Word_counter/time_manager.py
+++ Individual_Projects/Word_counter/time_manager.py
@@ -1,1 +1,9 @@
 #PS 1st this file finds the timestamp of when the document was last edited 
+
+import time as clock
+
+#timestamper
+def timestamp(file_name,word_count):
+    stamp = clock.ctime()
+    with open(file_name, "a") as file:
+        file.write(f"\nWord count: {word_count}, Last updated: {stamp}\n")
