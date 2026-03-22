135c5b55009f1fa833078e8b54e37ed48e13b6ff
titaniumlizard445 <parker.swenson@ucas-edu.net>
2026-03-21 23:47:13 -0600

    Add main menu for Geometry shape calculator

--- Individual_Projects/Geometry Calculator/main.py
+++ Individual_Projects/Geometry Calculator/main.py
@@ -1,5 +1,15 @@
 #PS 1st CP2 UI functions and running file
+from files.json_manager import *
+from sub_ui import *
 
 #main menu things
+@decorator
 def main_menu():
     print("Welcome to the Geometry shape calculator")
+    print(f"Shapes Created: {len(JSON_reader())}")
+    mode = stupid_proofed_inputs("Would you like to Use 2D mode or 3D mode?","title","2D","3D")
+    if mode == "2D":
+        second_dimension()
+
+
+main_menu()
