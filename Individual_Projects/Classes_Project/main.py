55288e286452963b671116c8820ed80110e632aa
titaniumlizard445 <parker.swenson@ucas-edu.net>
2026-03-23 20:02:48 -0600

    Fix main menu print statements and input handling

--- Individual_Projects/Classes_Project/main.py
+++ Individual_Projects/Classes_Project/main.py
@@ -5,11 +5,16 @@ from sub_ui import *
 #main menu things
 @decorator
 def main_menu():
+    
     print("Welcome to the Geometry shape calculator")
     print(f"Shapes Created: {len(JSON_reader())}")
-    mode = stupid_proofed_inputs("Would you like to Use 2D mode or 3D mode?","title","2D","3D").upper()
+    
+    mode = stupid_proofed_inputs("Would you like to Use 2D mode or 3D mode?","title","2D","3D")
+    
     if mode == "2D":
         second_dimension()
-
+    
+    elif mode == "3D":
+        third_dimension()
 
 main_menu()
