a5d78e71cca19c4d8fa112f07f7cf0d804eedc7d
titaniumlizard445 <parker.swenson@ucas-edu.net>
2026-04-06 18:39:42 -0600

    finished data visualization

--- Individual_Projects/Class_Relationships_Project/src/main.py
+++ Individual_Projects/Class_Relationships_Project/src/main.py
@@ -2,12 +2,12 @@
 
 #Import code
 from sub_ui import *
-
+from data_visualizing import *
 
 @decorator
 def main():
-    print("Welcome to the Character Creator\nChoose an Option:\n1. Create New Character\n2. View One Character's Information\n3. Level up Character\n4. Battle Characters\n5. View All Created Characters\n6. Equip/Remove Armor or Weapons from a character\n7. Assign New Abilities (The Weapon the Ability Uses must exist first before the ability can be made)\n8. Exit")
-    main_choice = stupid_proofed_inputs("Enter here: ","number","1","2","3","4","5","6","7")
+    print("Welcome to the Character Creator\nChoose an Option:\n1. Create New Character\n2. View One Character's Information\n3. Level up Character\n4. Battle Characters\n5. View All Created Characters\n6. Equip/Remove Armor or Weapons from a character\n7. Assign New Abilities (The Weapon the Ability Uses must exist first before the ability can be made)\n8. Use Graphs\n9. Exit")
+    main_choice = stupid_proofed_inputs("Enter here: ","number","1","2","3","4","5","6","7","8","9")
 
     match main_choice:
         case "1":
@@ -29,6 +29,8 @@ def main():
         case "7":
             create_ability()
         case "8":
-            print("Type No in Exit Prompt")
+            data_ui()
+        case "9":
+            print("Type no in exit prompt")
 
 main()
